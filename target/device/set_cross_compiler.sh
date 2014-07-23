bc7995164bf3ad1f16d0b8909631fa551311c823
RuifenShen <shenrfen@gmail.com>
2014-07-24 01:08:04 +0800

    - Change set_cross_compiler for multi-platform

--- target/device/set_cross_compiler.sh
+++ target/device/set_cross_compiler.sh
@@ -1,6 +1,18 @@
 ARDUINO_PATH="$HOME/arduino-1.5.3"
+MACHINE_TYPE=`getconf LONG_BIT`
+if [ $MACHINE_TYPE -eq 32 ]
+then
+  ARCH=i586
+else
+  if [ $MACHINE_TYPE -eq 64 ]
+  then
+    ARCH=x86_64
+  else
+     echo 'Error machine type:', $MACHINE_TYPE
+  fi
+fi
 SYSROOT="$ARDUINO_PATH/hardware/tools/sysroots/i586-poky-linux-uclibc"
-BIN_ROOT="$ARDUINO_PATH/hardware/tools/sysroots/x86_64-pokysdk-linux/usr/bin/i586-poky-linux-uclibc/i586-poky-linux-uclibc-"
+BIN_ROOT="$ARDUINO_PATH/hardware/tools/sysroots/$ARCH-pokysdk-linux/usr/bin/i586-poky-linux-uclibc/i586-poky-linux-uclibc-"
 export CC="${BIN_ROOT}gcc --sysroot=$SYSROOT -m32 -march=i586"
 export CXX="${BIN_ROOT}gcc --sysroot=$SYSROOT -m32 -march=i586"
 export LINK=$CXX
