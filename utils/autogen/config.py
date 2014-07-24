2414d166d0c6e91940ed67bc6cbb7a1808f5614b
zliu9 <zhanglin.liu@intel.com>
2014-07-24 20:34:47 +0800

    modified genShell modified config.py for mug

--- utils/autogen/config.py
+++ utils/autogen/config.py
@@ -1,5 +1,5 @@
 INPUT_DECL_PATHS  = [
-  "../../target/device/libio/export"
+  "../../../mug/include"
 #   "../../../pia-sdk-repo/iolib/arduino/arduiPIA.h"
 ]
 
@@ -19,6 +19,8 @@ EXPORT_MODULE        = 'galileo'
 
 GYP_PATH             = OUTPUT_DEV_PATH + '/../'
 GYP_SRC_PATH         = 'src/' #the relative path of GYP_SRC_PATH to OUTPUT_DEV_PATH from 
+GYP_LIB              = "'../../../../mug/bin/libmug.a'"
+GYP_INC              = ""
 GYP_FILE             = 'binding.gyp'
 Global_CLASS_VAR_FILE    = 'globalClassVarSetterGetter.js'
 INSTANCE_V8CLASS_ARG     = '__instanceV8' # used to generate a V8 class Object without
--- utils/autogen/genShell.py
+++ utils/autogen/genShell.py
@@ -15,6 +15,10 @@ def GenShell():
   printDbg("generating " + f)
   fp = open(f, "w+")
   s = '''
-OUTPUT_DIRS = %s %s
-''' % (GenOutputDirs(), INC_MAKEFILE)
+OUTPUT_DIRS = %s
+''' % (GenOutputDirs())
+
+  s += '''
+OUTPUT_FILES = %s %s %s
+''' % (GYP_PATH + GYP_FILE, GYP_PATH + Global_CLASS_VAR_FILE, INC_MAKEFILE)
   fp.write(s)
