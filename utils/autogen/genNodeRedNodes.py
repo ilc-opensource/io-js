2235137a4437a92d94370f42ba162ab3e4bacbff
zliu9 <zhanglin.liu@intel.com>
2015-06-03 10:07:17 +0800

    remove redundant <p> for help

--- utils/autogen/genNodeRedNodes.py
+++ utils/autogen/genNodeRedNodes.py
@@ -671,7 +671,7 @@ def GenGatheredNodeRedHtmlFile(NodeGroup, cppHeader):
 </script>
 
 <script type="text/x-red" data-help-name="%s">
-  <p> %s </p>
+  %s
 </script>
 ''' %(node, node, node, 
       AddIndent(configDefaultValueStr(node, configType, cppHeader), 6),
